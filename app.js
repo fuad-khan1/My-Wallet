8577aa2b6556ca327ca42d99c884bfdf32836ca0
Fuad Khan <fuadkhanantor1@gmail.com>
2022-02-21 07:25:04 +0600

    calculation modified

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-// Calculate Button
+// Calculate Button calculation****************************
+const incomeInput = document.getElementById("input-income");
 const foodInput = document.getElementById("input-food");
-  const rentInput = document.getElementById("input-rent");
-  const clothesInput = document.getElementById("input-clothes");
-  const displayExpense = document.getElementById("total-expense");
+const rentInput = document.getElementById("input-rent");
+const clothesInput = document.getElementById("input-clothes");
+const displayExpense = document.getElementById("total-expense");
+let balanceTotal = document.getElementById("balance-Total");
 
-  const incomeInput = document.getElementById("input-income");
-  let balanceTotal = document.getElementById("balance-Total");
 function calculateButton() {
-//   const foodInput = document.getElementById("input-food");
-//   const rentInput = document.getElementById("input-rent");
-//   const clothesInput = document.getElementById("input-clothes");
-//   const displayExpense = document.getElementById("total-expense");
+  if (foodInput.value >= 0 && rentInput.value >= 0 && clothesInput.value >= 0 && foodInput.value<incomeInput.value && rentInput.value<incomeInput.value && clothesInput.value<incomeInput.value) {
+    const totalExpenses =
+      Number(foodInput.value) +
+      Number(rentInput.value) +
+      Number(clothesInput.value);
+    displayExpense.innerText = totalExpenses;
+    balanceTotal.innerText = Number(incomeInput.value) - totalExpenses;
 
-  const totalExpenses =
-    Number(foodInput.value) +
-    Number(rentInput.value) +
-    Number(clothesInput.value);
-  displayExpense.innerText = totalExpenses;
-//   const incomeInput = document.getElementById("input-income");
-//   let balanceTotal = document.getElementById("balance-Total");
-  balanceTotal.innerText = Number(incomeInput.value) - totalExpenses;
+
+  } else {
+    const errorMsg = document.createElement("span");
+    errorMsg.innerText = "<Invalid input>";
+    document.getElementById("total-expense").appendChild(errorMsg);
+  }
+
 }
 
-//Save Button
+//Save Button calculation ****************************************
+const saveInput = document.getElementById("input-save");
+const savingAmount = document.getElementById("saving-amount");
+const remainingBalance = document.getElementById("remaining-balance");
+
 function saveButton() {
-  const saveInput = document.getElementById("input-save");
-  const savingAmount = document.getElementById("saving-amount");
-  const remainingBalance = document.getElementById("remaining-balance");
   savingAmount.innerText =
     (Number(saveInput.value) / 100) * balanceTotal.innerText;
+  remainingBalance.innerText = balanceTotal.innerText - savingAmount.innerText;
 }
-// document.getElementById('btn-save').addEventListener('click',function(){
-//     calculateButton()
-// })
